1ca399f594353fe345862d4f7b6974347f3bf1dd
Ning <wn0858@gmail.com>
2018-04-15 20:20:26 -0800

    migrate from bootstrap 3 to 4. it`s horrible

--- PropertyRentalManagement/App_Start/BundleConfig.cs
+++ PropertyRentalManagement/App_Start/BundleConfig.cs
@@ -29,7 +29,7 @@ namespace PropertyRentalManagement.App_Start
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 
